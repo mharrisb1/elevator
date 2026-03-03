bdc47cfd61e4f55131537fdd57219c28fbacac3d
Michael Harris <mharris@definite.app>
2026-03-02 21:51:22 -0700

    feat(strategy): add initial basic strategies

---
+++ src/elevator/strategy.py
@@ -0,0 +1,53 @@
+from enum import Enum
+from typing import Protocol
+
+
+class Strategy(Protocol):
+    def pick(self, current: int, remaining: list[int]) -> int:
+        """
+        Args:
+            current: Current floor
+            remaining: List of remaining floors to service
+        """
+        ...
+
+
+class FifoStragey(Strategy):
+    def pick(self, current: int, remaining: list[int]) -> int:  # noqa: ARG002, PLR6301
+        """Returns next floor in FIFO order"""
+        return remaining[0]
+
+
+class NearestFloorStrategy(Strategy):
+    def pick(self, current: int, remaining: list[int]) -> int:  # noqa: PLR6301
+        """Returns whichever floor is closest"""
+        return min(remaining, key=lambda f: abs(f - current))
+
+
+class Direction(Enum):
+    UP = 1
+    DOWN = -1
+
+    def toggle(self) -> "Direction":
+        """Opposite direction"""
+        return Direction(self.value * -1)
+
+
+class DirectionAwareNearestFloorStrategy(Strategy):
+    def __init__(self, direction: Direction = Direction.UP) -> None:
+        self._direction = direction
+
+    def pick(self, current: int, remaining: list[int]) -> int:
+        """
+        Returns the closes floor in the direction already going.
+        If no more floors are left in current direction then direction
+        changed
+        """
+        ahead = [f for f in remaining if (f - current) * self._direction.value > 0]
+        if not ahead:
+            self._direction = self._direction.toggle()
+            ahead = [f for f in remaining if (f - current) * self._direction.value > 0]
+
+        if not ahead:
+            return NearestFloorStrategy().pick(current, remaining)
+        return NearestFloorStrategy().pick(current, ahead)
---
+++ tests/unit/test_strategy.py
@@ -0,0 +1,57 @@
+import pytest
+
+from elevator.strategy import (
+    Direction,
+    DirectionAwareNearestFloorStrategy,
+    FifoStragey,
+    NearestFloorStrategy,
+)
+
+
+@pytest.mark.parametrize(
+    ("current", "remaining", "expected"),
+    [
+        (12, [2, 9, 1, 32], 2),
+        (1, [2, 3, 4, 5, 6, 7], 2),
+    ],
+)
+def test_fifo_strategy(current: int, remaining: list[int], expected: int):
+    next_floor = FifoStragey().pick(current, remaining)
+    assert next_floor == expected
+
+
+@pytest.mark.parametrize(
+    ("current", "remaining", "expected"),
+    [
+        (12, [2, 9, 1, 32], 9),
+        (1, [2, 3, 4, 5, 6, 7], 2),
+        (8, [2, 3, 4, 5, 6, 7], 7),
+    ],
+)
+def test_nearest_floor_strategy(current: int, remaining: list[int], expected: int):
+    next_floor = NearestFloorStrategy().pick(current, remaining)
+    assert next_floor == expected
+
+
+def test_direction_toggle():
+    direction = Direction.UP
+    assert direction.toggle() == Direction.DOWN
+
+
+@pytest.mark.parametrize(
+    ("direction", "current", "remaining", "expected"),
+    [
+        (Direction.UP, 12, [2, 9, 1, 32], 32),
+        (Direction.DOWN, 12, [2, 9, 13, 32], 9),
+        (Direction.UP, 12, [1, 2, 3, 4, 5], 5),
+        (Direction.UP, 12, [12], 12),
+    ],
+)
+def test_direction_aware_nearest_floor_strategy(
+    direction: Direction,
+    current: int,
+    remaining: list[int],
+    expected: int,
+):
+    next_floor = DirectionAwareNearestFloorStrategy(direction).pick(current, remaining)
+    assert next_floor == expected
